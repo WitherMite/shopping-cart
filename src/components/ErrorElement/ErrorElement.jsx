bb1b7dcff5bde6a86bacc0b696d2656b993ba0c5
WitherMite <43625516+WitherMite@users.noreply.github.com>
2024-12-30 14:34:38 -0600

    Display error element for failed fetch

--- src/components/ErrorElement/ErrorElement.jsx
+++ src/components/ErrorElement/ErrorElement.jsx
@@ -5,15 +5,26 @@ ErrorElement.propTypes = {
   error: PropTypes.instanceOf(Error),
 };
 
-export default function ErrorElement(error) {
-  const message = error?.message || "This page does not exist";
-  let solution = <Link to="/"> Return to Homepage </Link>;
+export default function ErrorElement({ error }) {
+  let message, solution;
 
-  // change solution depending on error cause
+  // change solution depending on error
+  switch (error.message) {
+    case "Failed to fetch": {
+      message = "Could not get items.";
+      solution = <Link to="/store"> Check internet and reload page</Link>;
+      break;
+    }
+    default: {
+      message = "This page does not exist";
+      solution = <Link to="/"> Return to Homepage </Link>;
+    }
+  }
+
   return (
-    <>
+    <div className="error-card">
       <h2>{message}</h2>
       <p>{solution}</p>
-    </>
+    </div>
   );
 }
--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -2,28 +2,34 @@ import { useOutletContext } from "react-router-dom";
 import { useEffect, useState } from "react";
 import ItemCard from "../ItemCard/ItemCard.jsx";
 import { addToCart } from "../../cart-helpers/cart-helpers.js";
+import ErrorElement from "../ErrorElement/ErrorElement.jsx";
 
 export default function Store() {
   const [cart, setCart] = useOutletContext();
   const [items, setItems] = useState([]);
   useEffect(() => {
-    // TODO: handle errors - render loading/error elements
+    // TODO: render loading elements
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
-      .then((json) => setItems(json));
+      .then((json) => setItems(json))
+      .catch((err) => {
+        setItems(<ErrorElement error={err} />);
+      });
   }, []);
 
   return (
     <>
       <h1>Store:</h1>
       <section className="store-grid-container">
-        {items.map((item) => (
-          <ItemCard
-            key={item.id}
-            {...item}
-            handleClick={(count) => addToCart(cart, item, count, setCart)}
-          />
-        ))}
+        {Array.isArray(items)
+          ? items.map((item) => (
+              <ItemCard
+                key={item.id}
+                {...item}
+                handleClick={(count) => addToCart(cart, item, count, setCart)}
+              />
+            ))
+          : items}
       </section>
     </>
   );
